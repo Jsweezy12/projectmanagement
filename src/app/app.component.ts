e225bdd409e89314aa0fcd6a4ac2101a8acd9ac1
john swann <john.swann@agapecoders.com>
2019-12-29 18:35:40 -0500

    Added login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'agprojectmanagement';
+
+  constructor(private router: Router){
+  
+  }
+
+  ngOnInit() {
+
+    this.router.navigate(['login'])
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HttpClientModule }    from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
+import { LoginpageComponent } from './components/loginpage/loginpage.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomepageComponent },
+  { path: '', component: LoginpageComponent },
+  {path:'login', component: LoginpageComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginpageComponent,
+    HomepageComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule
   ],
   providers: [],
